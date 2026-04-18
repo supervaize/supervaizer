a75402449cc5e4bc3f71553beb5c484883959182
Alain Prasquier <alain@supervaize.com>
2026-04-19 02:15:39 +0300

    Updated licence to 2026

--- tests/test_access_client_ip.py
+++ tests/test_access_client_ip.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2024-2026 Alain Prasquier - Supervaize.com. All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file, you can obtain one at
+# https://mozilla.org/MPL/2.0/.
+
 # Copyright (c) 2024-2025 Alain Prasquier - Supervaize.com. All rights reserved.
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
@@ -34,19 +40,27 @@ class TestExtractClientIpNoTrustedProxies:
         scope = _make_scope("1.2.3.4")
         assert _extract_client_ip(scope) == "1.2.3.4"
 
-    def test_ignores_xff_when_no_trusted_proxies(self: "TestExtractClientIpNoTrustedProxies") -> None:
+    def test_ignores_xff_when_no_trusted_proxies(
+        self: "TestExtractClientIpNoTrustedProxies",
+    ) -> None:
         scope = _make_scope("1.2.3.4", xff="9.9.9.9, 8.8.8.8")
         assert _extract_client_ip(scope) == "1.2.3.4"
 
-    def test_missing_client_returns_empty(self: "TestExtractClientIpNoTrustedProxies") -> None:
+    def test_missing_client_returns_empty(
+        self: "TestExtractClientIpNoTrustedProxies",
+    ) -> None:
         scope = {"type": "http", "client": None, "headers": []}
         assert _extract_client_ip(scope) == ""
 
-    def test_empty_peer_returns_empty(self: "TestExtractClientIpNoTrustedProxies") -> None:
+    def test_empty_peer_returns_empty(
+        self: "TestExtractClientIpNoTrustedProxies",
+    ) -> None:
         scope = {"type": "http", "client": ("", 0), "headers": []}
         assert _extract_client_ip(scope) == ""
 
-    def test_malformed_peer_returns_empty(self: "TestExtractClientIpNoTrustedProxies") -> None:
+    def test_malformed_peer_returns_empty(
+        self: "TestExtractClientIpNoTrustedProxies",
+    ) -> None:
         scope = _make_scope("not-an-ip")
         assert _extract_client_ip(scope) == ""
 
@@ -55,27 +69,39 @@ class TestExtractClientIpWithTrustedProxies:
     """When peer is in TRUSTED_PROXIES, use leftmost XFF entry."""
 
     @pytest.fixture(autouse=True)
-    def patch_trusted_proxies(self: "TestExtractClientIpWithTrustedProxies") -> Generator[None, None, None]:
+    def patch_trusted_proxies(
+        self: "TestExtractClientIpWithTrustedProxies",
+    ) -> Generator[None, None, None]:
         trusted = [ipaddress.ip_network("10.0.0.0/8")]
         with patch("supervaizer.access.client_ip.TRUSTED_PROXIES", trusted):
             yield
 
-    def test_peer_in_trusted_cidr_uses_xff(self: "TestExtractClientIpWithTrustedProxies") -> None:
+    def test_peer_in_trusted_cidr_uses_xff(
+        self: "TestExtractClientIpWithTrustedProxies",
+    ) -> None:
         scope = _make_scope("10.0.0.1", xff="203.0.113.5, 10.0.0.1")
         assert _extract_client_ip(scope) == "203.0.113.5"
 
-    def test_peer_not_in_trusted_cidr_ignores_xff(self: "TestExtractClientIpWithTrustedProxies") -> None:
+    def test_peer_not_in_trusted_cidr_ignores_xff(
+        self: "TestExtractClientIpWithTrustedProxies",
+    ) -> None:
         scope = _make_scope("1.2.3.4", xff="9.9.9.9")
         assert _extract_client_ip(scope) == "1.2.3.4"
 
-    def test_trusted_peer_no_xff_returns_peer(self: "TestExtractClientIpWithTrustedProxies") -> None:
+    def test_trusted_peer_no_xff_returns_peer(
+        self: "TestExtractClientIpWithTrustedProxies",
+    ) -> None:
         scope = _make_scope("10.0.0.2")
         assert _extract_client_ip(scope) == "10.0.0.2"
 
-    def test_trusted_peer_malformed_xff_falls_back_to_peer(self: "TestExtractClientIpWithTrustedProxies") -> None:
+    def test_trusted_peer_malformed_xff_falls_back_to_peer(
+        self: "TestExtractClientIpWithTrustedProxies",
+    ) -> None:
         scope = _make_scope("10.0.0.1", xff="not-an-ip, 10.0.0.1")
         assert _extract_client_ip(scope) == "10.0.0.1"
 
-    def test_trusted_peer_empty_xff_returns_peer(self: "TestExtractClientIpWithTrustedProxies") -> None:
+    def test_trusted_peer_empty_xff_returns_peer(
+        self: "TestExtractClientIpWithTrustedProxies",
+    ) -> None:
         scope = _make_scope("10.0.0.5", xff="  ")
         assert _extract_client_ip(scope) == "10.0.0.5"
